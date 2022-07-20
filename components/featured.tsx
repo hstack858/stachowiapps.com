f02f7a8a494f9565d5be6b388e632844e9eea102
Henry Stachowiak <stachowiak.h@northeastern.edu>
2022-07-19 22:05:00 -0500

    Change Featured Text

--- components/featured.tsx
+++ components/featured.tsx
@@ -20,8 +20,8 @@ const Featured: React.FC<FeaturedProps> = ({ openModal }) => (
       />
       <span className="my-4 text-lg">
         A college junior named Henry Stachowiak starts a sports betting site
-        with his genius roommate and faces the reality venture capital sharks.
-        What will happen to the shares he earned as the frontend lead?
+        with his genius roommate and faces the reality of venture capital
+        sharks. Will they crash and burn? Only time will tell.
       </span>
       <div className={styles.buttons}>
         <button
